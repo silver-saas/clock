751522f8f836c5832dd2e7dbab74a9f3bf20137d
Horia Coman <horia141@gmail.com>
2016-05-10 00:20:37 +0300

    Removed verbose output in curl gemfury script.

--- bin/togemfury.sh
+++ bin/togemfury.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 python setup.py sdist
-curl -F package=@dist/`ls dist` https://$GEMFURY_KEY@push.fury.io/silver-saas/ > result
+curl -s -F package=@dist/`ls dist` https://$GEMFURY_KEY@push.fury.io/silver-saas/ > result
 if [ -z "$(grep -e ok result)" ]
 then
   exit 1
